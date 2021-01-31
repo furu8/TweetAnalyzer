b7a883a6535febdc45777f4846470546b333c8e9
furu8 <foooolurk1125@gmail.com>
2021-01-31 17:52:19 +0900

    dockerわからん

---
+++ column/docker/start.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+JUPYTER_CONFIG=/root/.jupyter/jupyter_notebook_config.py
+
+PASSWORD=`python -c 'from notebook.auth import passwd;print(passwd())'`
+
+mkdir -p /root/.jupyter
+
+echo "c.NotebookApp.password = '$PASSWORD'" > JUPYTER_CONFIG
+
+jupyter notebook --no-browser --allow-root --ip 0.0.0.0 --notebook-dir $NOTEBOOK_DIR
